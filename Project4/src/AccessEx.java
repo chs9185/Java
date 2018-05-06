7e8f446e4c11cde9391289529759654f651357bb
채광식 <@192.168.29.15>
2018-05-06 14:43:54 +0900

    무시목록 설정.

---
+++ Project4/src/AccessEx.java
@@ -0,0 +1,18 @@
+class Sample{
+	public int a;
+	int b;
+	int c;
+}
+
+public class AccessEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sample aClass = new Sample();
+		aClass.a = 10;
+		aClass.b = 10;
+		aClass.c = 10;
+		
+	}
+
+}
